a46b94260c960ba426df1cb2f3eff490938e546c
Chesney Nyffels <chesney@nyffels.be>
2024-05-05 23:28:43 +0200

    Updated update logic

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,7 @@ RUN mkdir -p ./docker-monitor-worker-stats
 RUN mkdir -p ./docker-monitor-worker-notifications
 
 # Copy the programs
-COPY ./docker-monitor-worker-stats/. /docker-monitor-worker-stats/
-COPY ./docker-monitor-worker-notifications/. ./docker-monitor-worker-notifications/
-COPY ./entrypoint.sh ./entrypoint.sh
+COPY . .
 
 # Run the programs concurrently
 RUN npm i -g concurrently
--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,18 @@ gulp.task('default', function () {
   return gulp
     .src(['./docker-monitor-install/package.json'])
     .pipe(gulp.dest('./dist/docker-monitor-install'))
-    .pipe(
-      install({
-        args: ['--force'],
-      })
-    )
 
     .pipe(gulp.src(['./docker-monitor-worker-notifications/package.json']))
     .pipe(gulp.dest('./dist/docker-monitor-worker-notifications'))
-    .pipe(
-      install({
-        args: ['--force'],
-      })
-    )
 
     .pipe(gulp.src(['./docker-monitor-worker-stats/package.json']))
     .pipe(gulp.dest('./dist/docker-monitor-worker-stats'))
-    .pipe(
-      install({
-        args: ['--force'],
-      })
-
+    
     .pipe(gulp.src(['./Dockerfile']))
     .pipe(gulp.dest('./dist'))
     .pipe(gulp.src(['./.dockerignore']))
     .pipe(gulp.dest('./dist'))
-    );
+    
+    .pipe(gulp.src(['./entrypoint.sh']))
+    .pipe(gulp.dest('./dist'));
 });
