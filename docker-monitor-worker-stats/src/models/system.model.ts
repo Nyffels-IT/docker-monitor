273e0fc5b77cbcbcadfd7658d4ea5c0c058d281a
Chesney Nyffels <chesney@nyffels.be>
2024-05-08 01:22:50 +0200

    Added system model to stats

---
+++ docker-monitor-worker-stats/src/models/system.model.ts
@@ -0,0 +1,28 @@
+import { name, table, type } from "@nyffels/mysql2-extension";
+
+@table('system')
+export class System {
+  @name('system_id')
+  @type('string')
+  id: string;
+
+  @name('system_name')
+  @type('string')
+  name: string; 
+
+  @name('system_ssh_username')
+  @type('string')
+  sshUsername: string; 
+
+  @name('system_ssh_password')
+  @type('string')
+  sshPassword: string; 
+
+  @name('system_ssh_ip')
+  @type('string')
+  sshIp: string; 
+
+  @name('system_ssh_port')
+  @type('number')
+  sshPort: number;
+}
